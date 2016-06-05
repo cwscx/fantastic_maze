861aa2c737df1f5e992e884875ed12a086410c97
Albert Chang <axchang@ucsd.edu>
2016-06-04 19:38:58 -0700

    add goal sensor, setup laser scan and helper functions for movement

---
+++ scripts/helpers.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import rospy
+from math import *
+from geometry_msgs.msg import Pose, PoseArray, PointStamped, Quaternion, Point, Twist
+from fantastic_maze.msg import GoalSense
+
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+goal_sensor = rospy.Publisher('goal_sense', GoalSense, queue_size = 10)
+
+# hard coded constants: TODO modularize
+board_size = 10
+position = [0,4] # start position
+goal = [9,0]
+direction = 0
+
+# function to move robot one grid position forward in current angle. 
+def move():
+	twist = Twist()
+	twist.linear.x = 20
+	cmd_vel_pub.publish(twist)
+	rospy.sleep(1)
+	twist = Twist()
+	cmd_vel_pub.publish(twist)
+
+	#calculate new position and publish goal sensor values
+	if direction == 0:
+		position[0] += 1
+	elif direction == 90:
+		position[1] -= 1
+	elif direction == 180:
+		position[0] -= 1
+	elif direction == 270:
+		position[1] += 1
+	senseGoal()
+
+# turns robot parameter angle number of degrees, without moving. 
+def turn(angle):
+	global direction
+	direction = (direction + angle) % 360
+	senseGoal()
+
+	""" First Turn (in 10 degrees increments) """
+	angle_in_rad = float(angle*pi/180.0)
+	if (angle > 0):
+		increment = 10	
+	else:
+		increment = -10
+
+	increment_in_rad = float(increment*pi/180.0)
+	while angle_in_rad != 0:
+		twist = Twist()
+		""" Stage Doesn't Handle Large Turns accurately"""
+		if abs(angle_in_rad) > abs(increment_in_rad) :
+			twist.angular.z = increment_in_rad
+			angle_in_rad -= increment_in_rad
+		else:
+			twist.angular.z = angle_in_rad
+			angle_in_rad = 0
+
+		cmd_vel_pub.publish(twist)
+		rospy.sleep(1)
+		twist = Twist()
+		cmd_vel_pub.publish(twist)
+
+"""
+goal sensor, returns 4 boolean values depending upon whether the goal is within 
+half of the width of the entire gridsize in that direction.
+"""
+def senseGoal():
+	straight, back, left, right = False, False, False, False
+	xDist = abs(position[0] - goal[0])
+	yDist = abs(position[1] - goal[1])
+	if yDist < board_size / 2 and position[0] == goal[0]:
+		if direction == 90:
+			if position[1] - goal[1] > 0:
+				straight = True
+			else:
+				back = True
+		elif direction == 270:
+			if position[1] - goal[1] > 0:
+				back = True
+			else:
+				straight = True
+		elif direction == 0:
+			if position[1] - goal[1] > 0:
+				left = True
+			else:
+				right = True
+		elif direction == 180:
+			if position[1] - goal[1] > 0:
+				right = True
+			else:
+				left = True
+	elif xDist < board_size / 2 and position[1] == goal[1]:
+		if direction == 0:
+			if position[0] - goal[0] > 0:
+				back = True
+			else:
+				straight = True
+		elif direction == 90:
+			if position[0] - goal[0] > 0:
+				left = True
+			else:
+				right = True
+		elif direction == 180:
+			if position[0] - goal[0] > 0:
+				straight = True
+			else:
+				back = True
+		elif direction == 270:
+			if position[0] - goal[0] > 0:
+				left = False
+			else:
+				right = True
+
+	g = GoalSense()
+	g.straight = straight
+	g.back = back
+	g.left = left
+	g.right = right
+
+	goal_sensor.publish(g)
--- scripts/robot.py
+++ scripts/robot.py
@@ -2,8 +2,30 @@
 import rospy
 
 from maze_builder import Maze
+from helpers import *
+from sensor_msgs.msg import LaserScan
+from fantastic_maze.msg import GoalSense
 
+class Robot():
+	def __init__(self):
+		# maze = Maze(10)
+		rospy.init_node('robot')
+		self.laserSub = rospy.Subscriber('/base_scan', LaserScan, self.handleLaser)
+		self.goalScan = rospy.Subscriber('/goal_sense', GoalSense, self.handleGoal)
+		self.laser = LaserScan()	# class variable which will always contain the latest laser scan data
+		self.goals = GoalSense()	# class variable which will always contain the latest goal sensor data
+		rospy.sleep(2)
+		
+		move()
+		rospy.sleep(1)
+		turn(90)
+		rospy.sleep(1)
+		
+	def handleLaser(self, message):
+		self.laser = message
+
+	def handleGoal(self, message):
+		self.goals = message
 
 if __name__ == "__main__":
-	maze = Maze(10)
-	rospy.sleep(50)
+	r = Robot()
